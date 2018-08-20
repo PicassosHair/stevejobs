f6a7978db8f8832e462ebe884a381176531840ca
fuermosi777 <liuhao1990@gmail.com>
2018-08-20 00:27:09 -0700

    add stuff

---
+++ pedsparser/database.cpp
@@ -0,0 +1,9 @@
+//
+//  database.cpp
+//  pedsparser
+//
+//  Created by Hao Liu on 8/19/18.
+//  Copyright © 2018 Hao Liu. All rights reserved.
+//
+
+#include "database.hpp"
---
+++ pedsparser/database.hpp
@@ -0,0 +1,14 @@
+//
+//  database.hpp
+//  pedsparser
+//
+//  Created by Hao Liu on 8/19/18.
+//  Copyright © 2018 Hao Liu. All rights reserved.
+//
+
+#ifndef database_hpp
+#define database_hpp
+
+#include <stdio.h>
+
+#endif /* database_hpp */
