01cc7848fd428bf347742719e3ae501de030eacf
Hao <liuhao1990@gmail.com>
2019-03-22 17:05:49 -0700

    fix path?

--- jobs/download_latest_peds.sh
+++ jobs/download_latest_peds.sh
@@ -1,33 +1,33 @@
 #!/bin/bash
 # Download the latest data from https://ped.uspto.gov/peds/.
 
-DATA_DIR=${STORAGE_DIR}/peds
+DATA_DIR=/data/peds
 
 # Remove raw.old.zip if exists.
 rm -f DATA_DIR/raw.old.zip
 echo "Removed old raw.zip file."
 
 # Rename raw.zip to raw.old.zip if needed, or just download the data.
-if [ -e ${STORAGE_DIR}/raw.zip ]
+if [ -e /data/raw.zip ]
 then
-    mv ${STORAGE_DIR}/raw.zip ${STORAGE_DIR}/raw.old.zip
+    mv /data/raw.zip /data/raw.old.zip
     echo "Renamed raw.zip to raw.old.zip."
 else
     echo "Not found old raw.zip file."
-    touch ${STORAGE_DIR}/raw.zip
+    touch /data/raw.zip
 fi
 
 echo "Start downloading latest data."
-wget --output-document=${STORAGE_DIR}/raw.zip --show-progress https://ped.uspto.gov/api/full-download\?format\=JSON
+wget --output-document=/data/raw.zip --show-progress https://ped.uspto.gov/api/full-download\?format\=JSON
 
 if [ $? -eq 0 ]; then
     echo "Download complete!"
 
-    ${WORK_DIR}/bin/mail -sender="mailman@pathub.io" -subject="[PatHub Backend] PEDS is downloaded." -body="New bulk data is downloaded." -recipient="liuhao1990@gmail.com,hinmeng@gmail.com"
+    /usr/src/app/bin/mail -sender="mailman@pathub.io" -subject="[PatHub Backend] PEDS is downloaded." -body="New bulk data is downloaded." -recipient="liuhao1990@gmail.com,hinmeng@gmail.com"
 else
-    rm -rf ${STORAGE_DIR}/raw.zip
-    mv ${STORAGE_DIR}/raw.old.zip ${STORAGE_DIR}/raw.zip
+    rm -rf /data/raw.zip
+    mv /data/raw.old.zip /data/raw.zip
     echo "Downloading failed. Rolled back everything."
 
-    # ${WORK_DIR}/bin/mail -sender="mailman@pathub.io" -subject="[PatHub Backend] PEDS data downloading is failed." -body="New bulk data is NOT downloaded. Please check." -recipient="liuhao1990@gmail.com,hinmeng@gmail.com"
+    # /usr/src/app/bin/mail -sender="mailman@pathub.io" -subject="[PatHub Backend] PEDS data downloading is failed." -body="New bulk data is NOT downloaded. Please check." -recipient="liuhao1990@gmail.com,hinmeng@gmail.com"
 fi
