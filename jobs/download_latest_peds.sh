b46f4fd8c0168c65b83d9de3ce211fe0896fd27b
Hao <liuhao1990@gmail.com>
2019-03-28 14:05:09 -0700

    Improve downloading script

--- jobs/download_latest_peds.sh
+++ jobs/download_latest_peds.sh
@@ -6,30 +6,29 @@ APP_DIR=/usr/src/app
 RECIPIENT="liuhao1990@gmail.com,hinmeng@gmail.com"
 
 # Remove raw.old.zip if exists.
-rm -f ${DATA_DIR}/raw.old.zip
-echo "Removed old raw.zip file."
+if [ -e ${DATA_DIR}/raw.old.zip ]
+  rm -f ${DATA_DIR}/raw.old.zip
+  echo "Removed old raw.zip file."
+fi
 
 # Rename raw.zip to raw.old.zip if needed, or just download the data.
-if [ -e ${DATA_DIR}/raw.zip ]
-then
-    mv ${DATA_DIR}/raw.zip ${DATA_DIR}/raw.old.zip
-    echo "Renamed raw.zip to raw.old.zip."
+if [ -e ${DATA_DIR}/raw.zip ] then
+  echo "Renamed raw.zip to raw.old.zip."
+  mv ${DATA_DIR}/raw.zip ${DATA_DIR}/raw.old.zip
 else
-    echo "Not found old raw.zip file."
-    touch ${DATA_DIR}/raw.zip
+  echo "Not found old raw.zip file. Create a fake raw.zip for placeholder."
+  touch ${DATA_DIR}/raw.zip
 fi
 
 echo "Start downloading latest data."
-wget --output-document=${DATA_DIR}/raw.zip --show-progress https://ped.uspto.gov/api/full-download\?format\=JSON
+wget --output-document=${DATA_DIR}/raw.zip https://ped.uspto.gov/api/full-download\?format\=JSON
 
 if [ $? -eq 0 ]; then
     echo "Download complete!"
 
     ${APP_DIR}/bin/mail -subject="[PatHub Backend] PEDS is downloaded." -body="New bulk data is downloaded." -recipient=${RECIPIENT}
 else
-    rm -rf ${DATA_DIR}/raw.zip
-    mv ${DATA_DIR}/raw.old.zip ${DATA_DIR}/raw.zip
-    echo "Downloading failed. Rolled back everything."
+    echo "Downloading failed."
 
-    ${APP_DIR}/bin/mail -subject="[PatHub Backend] PEDS data downloading is failed." -body="New bulk data is NOT downloaded. Please check." -recipient=${RECIPIENT}
+    ${APP_DIR}/bin/mail -subject="[PatHub Backend] PEDS data downloading is failed." -body="PEDS bulk data is NOT downloaded. Please check." -recipient=${RECIPIENT}
 fi
