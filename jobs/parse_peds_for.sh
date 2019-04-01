6f3ac611c33561e0cccb0b94a248256e21f05a58
Hao <liuhao1990@gmail.com>
2019-04-01 12:22:52 -0700

    update parse peds for year

--- jobs/parse_peds_for.sh
+++ jobs/parse_peds_for.sh
@@ -7,18 +7,27 @@ set -e
 DATA_DIR=/data
 APP_DIR=/usr/src/app
 RECIPIENT="liuhao1990@gmail.com,hinmeng@gmail.com"
-
-# Put MySQL credentials into $HOME/config/mysql.conf
 YEAR=$1
 START_TIME=`date +%s`
+START_DATE=`date +%Y%m%d`
 
 # Prepare work. Remove temp folder if exists.
 rm -rf ${DATA_DIR}/temp
 mkdir -p ${DATA_DIR}/temp
 
+# Check for raw.YYYYMMDD.zip file existance.
+if [ -e ${DATA_DIR}/raw.${START_DATE}.zip ] 
+then
+  echo "Detect raw.${START_DATE}.zip existing."
+else
+  echo "Error. raw.${START_DATE}.zip doesn't exist. Stop parsing."
+  exit 1
+fi
+
+
 # Unzip raw.json to temp/YYYY.json
-echo "Unzipping raw.zip. for year ${YEAR}"
-unzip -o ${DATA_DIR}/raw.zip $1.json -d ${DATA_DIR}/temp/
+echo "Unzipping raw.${START_DATE}.zip. for year ${YEAR}"
+unzip -o ${DATA_DIR}/raw.${START_DATE}.zip $1.json -d ${DATA_DIR}/temp/
 
 if [ $? -ne 0 ]; then
     echo "Unzip failed."
@@ -36,15 +45,12 @@ fi
 
 # Generate raw load application SQL file.
 # Assumption: these shell scripts are in the same directory.
-bash ${APP_DIR}/jobs/insert_to_database.sh application ${DATA_DIR}/temp/applications ${YEAR}
+# ${APP_DIR}/jobs/insert_to_database.sh application ${DATA_DIR}/temp/applications ${YEAR}
 
 # Generate raw load codes SQL file.
-bash ${APP_DIR}/jobs/insert_to_database.sh code ${DATA_DIR}/temp/codes ${YEAR}
+# ${APP_DIR}/jobs/insert_to_database.sh code ${DATA_DIR}/temp/codes ${YEAR}
 
 # Generate raw load codes SQL file.
-bash ${APP_DIR}/jobs/insert_to_database.sh transaction ${DATA_DIR}/temp/transactions ${YEAR}
+# ${APP_DIR}/jobs/insert_to_database.sh transaction ${DATA_DIR}/temp/transactions ${YEAR}
 
-# Clean up work.
-rm -rf ${DATA_DIR}/temp
-
-echo "Done! Used $(expr `date +%s` - $START_TIME) s"
+echo "Done parsing data for year ${YEAR}! Used $(expr `date +%s` - $START_TIME) s."
