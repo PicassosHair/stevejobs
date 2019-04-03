0f1cb354de5bef80fcaa91e1f1ed90d4cad39f69
Hao <liuhao1990@gmail.com>
2019-04-03 11:07:08 -0700

    fix a mossing ext

--- jobs/testing_job.sh
+++ jobs/testing_job.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "This is a testing job that does not do anything"
-/usr/src/app/jobs/log_slack info "Testing info msg."
-/usr/src/app/jobs/log_slack success "Testing ok msg."
-/usr/src/app/jobs/log_slack warning "Testing warning msg."
-/usr/src/app/jobs/log_slack error "Testing bad msg."
+/usr/src/app/jobs/log_slack.sh info "Testing info msg."
+/usr/src/app/jobs/log_slack.sh success "Testing ok msg."
+/usr/src/app/jobs/log_slack.sh warning "Testing warning msg."
+/usr/src/app/jobs/log_slack.sh error "Testing bad msg."
