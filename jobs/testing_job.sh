bb1f46656861b36da09732e5be6988ce516fff62
Hao <liuhao1990@gmail.com>
2019-04-03 11:48:40 -0700

    update slack

--- jobs/testing_job.sh
+++ jobs/testing_job.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+APP_DIR=/usr/src/app
 S="${APP_DIR}/jobs/log_slack.sh"
+SLAC=/usr/src/app/jobs/log_slack.sh
 
 echo "This is a testing job that does not do anything"
 ${APP_DIR}/jobs/log_slack.sh info "Test okay msg."
 
 $S info "Okay."
+$SLAC error "Erro."
