797271f364b7a25f4feb080d944dac7283e2f60a
Hao <liuhao1990@gmail.com>
2019-04-03 11:40:10 -0700

    update slack

--- jobs/parse_peds_for.sh
+++ jobs/parse_peds_for.sh
@@ -16,35 +16,35 @@ LOG_SLACK=${APP_DIR}/jobs/log_slack.sh
 rm -rf ${DATA_DIR}/temp
 mkdir -p ${DATA_DIR}/temp
 
-LOG_SLACK info "Parse PEDS data for year ${YEAR}."
+${LOG_SLACK} info "Parse PEDS data for year ${YEAR}."
 
 # Check for latest raw.YYYYMMDD.zip file existance.
 LATEST_RAW_ZIP=`ls ${DATA_DIR}/*.zip -t | head -n 1`
 
 if [ -e ${LATEST_RAW_ZIP} ] 
 then
-  LOG_SLACK info "Found raw data: ${LATEST_RAW_ZIP}, continue."
+  ${LOG_SLACK} info "Found raw data: ${LATEST_RAW_ZIP}, continue."
 else
-  LOG_SLACK error "Error. Latest raw zip file doesn't exist. Stop parsing for year ${YEAR}."
+  ${LOG_SLACK} error "Error. Latest raw zip file doesn't exist. Stop parsing for year ${YEAR}."
   exit 1
 fi
 
 
 # Unzip raw.json to temp/YYYY.json
-LOG_SLACK info "Unzipping raw.${START_DATE}.zip. for year ${YEAR}."
+${LOG_SLACK} info "Unzipping raw.${START_DATE}.zip. for year ${YEAR}."
 unzip -o ${LATEST_RAW_ZIP} $1.json -d ${DATA_DIR}/temp/
 
 if [ $? -ne 0 ]; then
-    LOG_SLACK error "Unzip failed for year ${YEAR}."
+    ${LOG_SLACK} error "Unzip failed for year ${YEAR}."
     exit 1
 fi
 
 # Parse json file to temp/applications, temp/codes, temp/transactions, which are csv-like files.
-LOG_SLACK info "Parsing ${YEAR}.json."
+${LOG_SLACK} info "Parsing ${YEAR}.json."
 ${APP_DIR}/bin/parser -in=${DATA_DIR}/temp/$YEAR.json -out=${DATA_DIR}/temp
 
 if [ $? -ne 0 ]; then
-    LOG_SLACK error "Parsing ${YEAR}.json failed."
+    ${LOG_SLACK} error "Parsing ${YEAR}.json failed."
     exit 1
 fi
 
@@ -57,4 +57,4 @@ ${APP_DIR}/jobs/insert_to_database.sh code ${DATA_DIR}/temp/codes ${YEAR}
 # Generate raw load codes SQL file.
 ${APP_DIR}/jobs/insert_to_database.sh transaction ${DATA_DIR}/temp/transactions ${YEAR}
 
-LOG_SLACK success "Done parsing data for year ${YEAR}! Used $(expr `date +%s` - $START_TIME) s."
+${LOG_SLACK} success "Done parsing data for year ${YEAR}! Used $(expr `date +%s` - $START_TIME) s."
--- jobs/testing_job.sh
+++ jobs/testing_job.sh
@@ -3,4 +3,4 @@
 LOG_SLACK=${APP_DIR}/jobs/log_slack.sh
 
 echo "This is a testing job that does not do anything"
-LOG_SLACK info "Test okay msg."
+${LOG_SLACK} info "Test okay msg."
